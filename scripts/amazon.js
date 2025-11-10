c7c491220966ce02dd862d4acc8414a0ea6b6787
Longdev1701 <n23dccn035@student.ptithcm.edu.vn>
2025-11-10 23:43:06 +0700

    Make paymentSummary interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
 import {cart, addToCart,calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import { renderOrderSummary } from './checkout/orderSummary.js';
 
 let productsHTML = '';
 
@@ -96,5 +97,6 @@ document.querySelectorAll('.js-add-to-cart-button')
 			}, 2000);
 
 			addedMessageTimeouts[productId] = timeoutId;
+
 		});
 	});
