24b8738e154b819660ba9b43b44a3a6a40134cc5
Longdev1701 <n23dccn035@student.ptithcm.edu.vn>
2025-11-14 14:31:51 +0700

    Create automated for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,22 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite: formatCurrency')
+
+if (formatCurrency(2095) === '20.95'){
+    console.log('pass');
+}else{
+    console.log('false');
+}
+
+if (formatCurrency(0) === '0.00'){
+    console.log('pass');
+}else{
+    console.log('false');
+}
+
+if(formatCurrency(2000.5) === '20.01'){
+    console.log('pass');
+}
+else{
+    console.log('false');
+}
