c87485c22ca3e35af47b01e54f5b4408c912d160
Longdev1701 <n23dccn035@student.ptithcm.edu.vn>
2025-11-19 13:41:48 +0700

    Make tracking bar interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -25,6 +25,11 @@ async function renderTrackingPage(){
     
     console.log(order);
 
+    const today = dayjs();
+    const orderTime = dayjs(order.orderTime);
+    const deliveryTime = dayjs(productInfo.estimatedDeliveryTime);
+    const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
     document.querySelector('.js-order-tracking')
         .innerHTML = `
             <a class="back-to-orders-link link-primary" href="orders.html">
@@ -46,21 +51,23 @@ async function renderTrackingPage(){
             <img class="product-image" src="${product.image}">
 
             <div class="progress-labels-container">
-            <div class="progress-label">
+            <div class="progress-label ${percentProgress < 50 ? 'current-status' :''}">
                 Preparing
             </div>
-            <div class="progress-label current-status">
+            <div class="progress-label ${(percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''}">
                 Shipped
             </div>
-            <div class="progress-label">
+            <div class="progress-label ${(percentProgress >= 100) ? 'current-status':''}">
                 Delivered
             </div>
             </div>
 
             <div class="progress-bar-container">
-            <div class="progress-bar"></div>
+            <div class="progress-bar" style="width: ${percentProgress}%;"></div>
             </div>
         `;
+
 }
 
 renderTrackingPage();
+
